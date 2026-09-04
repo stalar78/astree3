0ca03cc70b1a4f55279015ea9fb88a62d3be6b9c
Stanislav <104514094+stalar78@users.noreply.github.com>
2026-09-04 09:00:23 +0300

    feat: validate frontend edition at startup

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './config/edition';
 import './styles.css';
 import { routes } from './routes';
 
