d9f22039dd18b732502217e5986082e557398d32
Stanislav <104514094+stalar78@users.noreply.github.com>
2026-09-04 21:06:49 +0300

    feat: expose candidate questionnaire in hosting preview

--- frontend/src/pages/CandidateUnavailablePage.tsx
+++ frontend/src/pages/CandidateUnavailablePage.tsx
@@ -1,13 +1,5 @@
-import { ReferenceInnerPage, ReferenceNotice } from '../components/ReferenceInnerPage';
-
-const INACTIVE_COPY = 'Приём заявок через сайт временно недоступен.';
+import { CandidatePage } from './CandidatePage';
 
 export function CandidateUnavailablePage() {
-  return (
-    <ReferenceInnerPage eyebrow="Кандидату" title="Вступление" lead={INACTIVE_COPY}>
-      <div className="mx-auto w-full max-w-5xl">
-        <ReferenceNotice title="Приём заявок">{INACTIVE_COPY}</ReferenceNotice>
-      </div>
-    </ReferenceInnerPage>
-  );
+  return <CandidatePage />;
 }
